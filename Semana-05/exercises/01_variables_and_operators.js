dd354d0d9ae98c2cf5a1ed1a9cc6a12a989539e2
DamianPalavecino <damianpalavecino25@gmail.com>
2022-09-20 11:18:40 -0300

    BaSP-W05-2022: ex01 - Variables and Operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,24 @@
+// 1. Variables and Operators
+
+/* 1a) Create two integer variables and use the add operator to
+save the value of the addition of both on a third variable. */
+
+var firstNumber = 2;
+var secondNumber = 3;
+var addition = firstNumber + secondNumber;
+console.log("1A) 2 + 3 =", addition);
+
+// 1b) Create two variables of String type and concatenate them together and save the result on a third variable.
+
+var firstString = "Hello";
+var secondString = " World!";
+var concatString = firstString + secondString;
+console.log("1B) Hello +  World! =", concatString);
+
+/* 1c) Create two variables of String type and add the length of each variable (number of letters of the string)
+saving the result of the sum in a third variable (using length).*/
+
+var firstName = "Damian";
+var lastName = "Palavecino";
+var nameLength = firstName.length + lastName.length;
+console.log("1C) The sum of the lenght of Damian + Palavecino is", nameLength);
